71f1f9fdefe1b6a3d5cd5bf5a5ba6f80e93965a9
sebseb7 <github@seb.exse.net>
2013-03-06 13:28:23 +0100

    isp

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/*
+ * 	datasheet: http://www.nxp.com/documents/data_sheet/LPC81XM.pdf
+ *  user manual: http://www.nxp.com/documents/user_manual/UM10601.pdf
+ *
+ *  ISP: When pin PIO0_1 is pulled LOW on reset, the part enters ISP mode and the ISP command handler starts up. 
+ *       In ISP mode, pin PIO0_0 is connected to function U0_RXD and pin PIO0_4 is connected to function U0_TXD on the USART0 block.
+ */
+
 
 volatile uint32_t msTicks = 0;
 
