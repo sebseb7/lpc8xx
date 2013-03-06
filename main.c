8dcfb41a4cd64f4c724cdb7fcac8ad82694f3aae
sebseb7 <github@seb.exse.net>
2013-03-06 14:51:33 +0100

    switch tool

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 /*
  * 	datasheet: http://www.nxp.com/documents/data_sheet/LPC81XM.pdf
  *  user manual: http://www.nxp.com/documents/user_manual/UM10601.pdf
+ *  swtich matrix tool: http://www.lpcware.com/content/nxpfile/nxp-switch-matrix-tool-lpc800 (for pin configuration)
+ *
  *
  *  ISP: When pin PIO0_1 is pulled LOW on reset, the part enters ISP mode and the ISP command handler starts up. 
  *       In ISP mode, pin PIO0_0 is connected to function U0_RXD and pin PIO0_4 is connected to function U0_TXD on the USART0 block.
